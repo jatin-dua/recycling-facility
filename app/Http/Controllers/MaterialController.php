c1998d15e1ceb4d4f5c7e38d05371e62ae57fcae
Jatin Dua <jdua004@gmail.com>
2025-08-27 22:51:38 +0530

    feat: Add MaterialController

---
+++ app/Http/Controllers/MaterialController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Material;
+use Illuminate\Http\Request;
+
+class MaterialController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Material $material)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Material $material)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Material $material)
+    {
+        //
+    }
+}
